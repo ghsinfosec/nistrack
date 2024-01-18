18caf36577a91afdf586558537cf1d674ac43a4b
Heath Stewart <heath.stewart1@gmail.com>
2024-01-18 13:34:53 -0500

    changing h1 to h2 for function names

--- views/functions-html.php
+++ views/functions-html.php
@@ -15,7 +15,7 @@ $functionsHTML = "<div class='nist-functions'>";
 while ($func = $functions->fetchObject()) {
 	$functionsHTML .= "
 		<article class='functions'>
-			<h1>$func->function_name</h1>
+			<h2>$func->function_name</h1>
 			<p>$func->function_description</p>
 		</article>
 		";
