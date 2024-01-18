4d314d18ace217400e5369d47cad58b5c5458e15
Heath Stewart <heath.stewart1@gmail.com>
2024-01-18 11:09:05 -0500

    removed functions.php

--- views/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return "
-<h1>NIST Functions</h1>
-<p>Function info will go here..</p>
-";
--- views/navigation.php
+++ views/navigation.php
@@ -3,6 +3,6 @@
 return "
 <nav>
 	<a href='index.php?page=about'>About</a>
-	<a href='index.php?page=functions'>Functions</a>
+	<a href='index.php?page=login'>Login</a>
 </nav>
 ";
