99bbb790f5dfbacc5d97e4e53f94ad71ca185a2d
Heath Stewart <heath.stewart1@gmail.com>
2024-01-19 16:29:12 -0500

    adding js elements

--- models/PageData.class.php
+++ models/PageData.class.php
@@ -5,6 +5,11 @@ class PageData {
 	public string $title = "";
 	public string $content = "";
 	public string $css = "";
+	public string $scriptElements = "";
+
+	public function addScript($src) {
+		$this->scriptElements = "<script src='$src' defer></script>";
+	}
 
 	public function addCSS($href) {
 		$this->css .= "<link href='$href' rel='stylesheet'>";
