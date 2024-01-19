a6d629a4e5729e0471daf48bbc8daffeb371c008
Heath Stewart <heath.stewart1@gmail.com>
2024-01-19 16:29:34 -0500

    including js file

--- views/page.php
+++ views/page.php
@@ -8,6 +8,7 @@ return "
 		<title>$pageData->title</title>
 		<meta http-equiv='Content-Type' content='text/html;charset=utf-8'/>
 		$pageData->css
+		$pageData->scriptElements
 	</head>
 	<body>
 		$pageData->content
